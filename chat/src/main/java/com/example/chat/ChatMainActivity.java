b016bd3a3bfca724efed17019185067b186a0473
nby <962740589@qq.com>
2022-11-15 20:33:26 +0800

    first commit!

---
+++ chat/src/main/java/com/example/chat/ChatMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.chat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChatMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_main);
+    }
+}
