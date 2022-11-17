20455e6e63e2665d02d827a6fbd4590ec989754f
nby <962740589@qq.com>
2022-11-17 21:18:10 +0800

    构建组件化

--- app/src/main/java/com/example/qianqian/MainActivity.java
+++ app/src/main/java/com/example/qianqian/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.qianqian;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.chat.ChatMainActivity;
+import com.example.myapplication.SquareMainActivity;
+import com.example.personal.PersonalMainActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goChat(View view) {
+        startActivity(new Intent(this, ChatMainActivity.class));
+    }
+
+    public void goSquare(View view) {
+        startActivity(new Intent(this, SquareMainActivity.class));
+    }
+
+    public void goPersonal(View view) {
+        startActivity(new Intent(this, PersonalMainActivity.class));
     }
 }
--- chat/src/main/java/com/example/chat/ChatMainActivity.java
+++ chat/src/main/java/com/example/chat/ChatMainActivity.java
@@ -10,5 +10,7 @@ public class ChatMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_main);
+
+
     }
 }
